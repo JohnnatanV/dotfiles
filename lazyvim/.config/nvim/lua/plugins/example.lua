f01aa88b283c3941ed7452e6398755f67e7dcb02
JohnnatanV <joh.villaneda@outlook.com>
2025-03-08 21:49:45 -0500

    Update lazyvim config

--- lazyvim/.config/nvim/lua/plugins/example.lua
+++ lazyvim/.config/nvim/lua/plugins/example.lua
@@ -194,4 +194,14 @@ return {
       },
     },
   },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
 }
